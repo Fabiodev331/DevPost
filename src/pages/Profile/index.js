928f155c73c45f605233a5f2ce2ae53da0a89eef
devprogramador <sjosefabio1996@gmail.com>
2023-06-06 23:29:37 -0300

    Criando página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,23 +1,61 @@
-import React, { useContext } from "react";
-import { View, Text, Button } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text} from "react-native";
 
 import { AuthContext } from "../../contexts/auth";
+import { 
+    Container, 
+    Name, 
+    Email, 
+    Button, 
+    ButtonText,
+    UploadButton,
+    UploadText,
+    Avatar,
+
+} from "./styles";
+
+import Header from "../../componets/Header";
 
 function Profile(){
-    const { signOut } = useContext(AuthContext)
+    const { signOut, user } = useContext(AuthContext)
+
+    const [nome, setNome] = useState(user?.nome);
+    const [url, setUrl] = useState('https://sujeitoprogramador.com/steve.png');
 
     async function handleSignOut(){
         signOut();
     }
 
     return(
-        <View>
-            <Text>Tela perfil</Text>
-            <Button
-                title="Sair"
-                onPress={handleSignOut}
-            />
-        </View>
+        <Container>
+            <Header/>
+
+            {url ? (
+                <UploadButton onPress={() => alert('clicou1')} >
+                    <UploadText>+</UploadText>
+                    <Avatar
+                        source={{uri: url}}
+                    />
+
+                </UploadButton>
+            ) : (
+                <UploadButton onPress={() => alert('clicou2')}>
+                    <UploadText>+</UploadText>
+                </UploadButton>
+            )}
+
+            <Name>{user?.nome}</Name>
+            <Email>{user?.email}</Email>
+
+            <Button bg="#428cfd">
+                <ButtonText color="#FFF" >Atualizar perfil</ButtonText>
+            </Button>
+
+            <Button bg="#DDD" onPress={handleSignOut} >
+                <ButtonText color="#353840" >Sair</ButtonText>
+            </Button>
+
+        </Container>
     )
 }
 
